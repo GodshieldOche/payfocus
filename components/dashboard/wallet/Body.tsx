fea0ca7257a2c8a479cadd9707996872a60244c1
GodshieldOche <goddyartz0258@gmail.com>
2022-09-26 16:44:20 +0100

    29th commit

--- components/dashboard/wallet/Body.tsx
+++ components/dashboard/wallet/Body.tsx
@@ -10,13 +10,13 @@ const Body = () => {
     <div className='w-full h-full pb-24 !pt-[69px] px-4 sm:px-[43px] lg:px-6 space-y-6'>
         <h1 className=' text-base lg:text-xl text-primaryOne  '>Recent Transactions</h1>
         <Tab />
-        <>
+        <div className='space-y-4'>
          {
             array.map((iem: any) => (
                 <Transaction key={iem} />
             ))
         }
-        </>
+        </div>
     </div>
   )
 }
