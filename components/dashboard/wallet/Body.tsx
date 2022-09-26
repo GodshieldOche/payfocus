948412f1bf78c38a0f729dafb68573404e672036
GodshieldOche <goddyartz0258@gmail.com>
2022-09-26 16:39:51 +0100

    28th commit

--- components/dashboard/wallet/Body.tsx
+++ components/dashboard/wallet/Body.tsx
@@ -7,7 +7,7 @@ const array  = [1,2,3,4,5,6,7, 8, 9, 10]
 
 const Body = () => {
   return (
-    <div className='w-full h-full pb-24 !pt-[69px] px-3 sm:px-4 lg:px-6 space-y-6'>
+    <div className='w-full h-full pb-24 !pt-[69px] px-4 sm:px-[43px] lg:px-6 space-y-6'>
         <h1 className=' text-base lg:text-xl text-primaryOne  '>Recent Transactions</h1>
         <Tab />
         <>
