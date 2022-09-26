701ffab9b2e1ee9c11ea5a042e79139a1e3f1d79
GodshieldOche <goddyartz0258@gmail.com>
2022-09-26 16:33:48 +0100

    27th commit

--- components/common/Toggle.tsx
+++ components/common/Toggle.tsx
@@ -19,14 +19,17 @@ const Toggle : React.FC<Props> = ({ }) => {
 
   return (
     <div className='flex items-center w-full space-x-1 py-1 px-1 bg-secondaryOne dark:bg-darkOne rounded-[15px] '>
-
-      <div 
+{
+  mounted 
+  &&
+  <>
+    <div 
         onClick={() =>
           setTheme("dark")
       }
       className={` ${resolvedTheme === 'dark' ? "bg-primaryOne " : "bg-transparent hover:bg-mainBlack/10 "} 
         cursor-pointer px-5 py-2 xl:px-6 xl:py-3 rounded-[10px]`}>
-        <Icon icon='bxs:moon' className={` ${resolvedTheme === 'dark' ? "!text-light " : "!text-mainBlack "} !text-lg `} />
+        <Icon icon='bxs:moon' className={` ${resolvedTheme === 'dark' ? "text-light " : "text-mainBlack "} !text-lg `} />
       </div>
 
       <div 
@@ -34,8 +37,11 @@ const Toggle : React.FC<Props> = ({ }) => {
           setTheme("light")
       }
         className={`${resolvedTheme === 'dark' ? "bg-transparent " : "bg-primaryOne "} cursor-pointer px-5 py-2 xl:px-6 xl:py-3 rounded-[10px]`}>
-        <Icon icon='carbon:light-filled' className={`${resolvedTheme === 'dark' ? "!text-mainBlack " : "!text-light "} !text-lg `} />
+        <Icon icon='carbon:light-filled' className={`${resolvedTheme === 'dark' ? "text-mainBlack " : "text-light "} !text-lg `} />
       </div>
+  </> 
+}
+      
     </div>
   )
 }
