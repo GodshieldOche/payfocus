024980cbb76675f5775c538a6e07b16b78053bc7
GodshieldOche <goddyartz0258@gmail.com>
2022-09-28 15:46:35 +0100

    37th commit

--- components/common/BackNav.tsx
+++ components/common/BackNav.tsx
@@ -20,7 +20,9 @@ const BackNav: React.FC<Props> = ({text}) => {
       <h1 className='text-light !text-center'>{text}</h1>
 
       <div className='absolute lg:hidden h-full top-0 bottom-0 my-auto left-0 sm:left-4 flex flex-col justify-center'>
-        <HiOutlineArrowLeft className='text-lg text-light xl:!text-xl cursor-pointer' />
+        <HiOutlineArrowLeft 
+         onClick={()=> router.back()}
+        className='text-lg text-light xl:!text-xl cursor-pointer' />
       </div>
     </div>
   )
--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import IconLink from '../common/Icon'
 
 const Footer = () => {
   return (
-    <div className='w-full absolute bottom-0 left-0 right-0 bg-[#f5f5f5] dark:bg-dark px-4 sm:px-[43px] py-2 z-40
+    <div className='w-full fixed bottom-0 left-0 right-0 bg-[#f5f5f5] dark:bg-dark px-4 sm:px-[43px] py-2 z-40
     flex items-center justify-between lg:hidden'>
         <IconLink active={true} text='Wallet' icon="entypo:wallet" />
         <IconLink active={false} text='Payments' icon="material-symbols:payments-outline-rounded" />
